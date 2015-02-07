891c5aea2fde1ec105c0b9294b9ed6b6f45dae2f
KatieCat <jankowski.kathryna@gmail.com>
2015-02-07 11:00:42 -0500

    Added other Groups Tests

--- src/junit/TestTypeOfTriangle.java
+++ src/junit/TestTypeOfTriangle.java
@@ -9,18 +9,33 @@ import triangle.TypeOfTriangle.TriangleTypes;
 
 public class TestTypeOfTriangle {
 
-	@Test
-	public void TestZeroValue()
+	@Test //Assignment 2 Test
+	public void TestSideEquals0()
 	{
 	    TypeOfTriangle triangle = new TypeOfTriangle(3, 4, 0);
 
 	    assertEquals( TriangleTypes.NONE, triangle.GetTypeOfTriangle());
 	}
 
-	@Test
+	@Test //Assignment 2 Test
 	public void TestNegativeValue()
 	{
 	    TypeOfTriangle triangle = new TypeOfTriangle(3, 4, -5);
+
+	    assertEquals( TriangleTypes.NONE, triangle.GetTypeOfTriangle());
+	}
+	
+	@Test //Assignment 2 Test
+	public void TestNoValue()
+	{
+		TypeOfTriangle triangle = new TypeOfTriangle(3, 4);
+		//Code Needed
+	}
+	
+	@Test //Assignment 2 Test
+	public void TestLargeValue()
+	{
+	    TypeOfTriangle triangle = new TypeOfTriangle(3, 4, 2147483647);
 
 	    assertEquals( TriangleTypes.NONE, triangle.GetTypeOfTriangle());
 	}
@@ -65,5 +80,72 @@ public class TestTypeOfTriangle {
 		
 		assertTrue( triangle.IsRight() );
 		
+	}
+	
+	//Group 1 Tests
+	
+	@Test
+	public void Test1()
+	{
+		TypeOfTriangle triangle = new TypeOfTriangle(5, 6, 6);
+		
+		assertFalse( triangle.IsRight() );
+		
+		assertEquals( TriangleTypes.ISOSCELES, triangle.GetTypeOfTriangle() );
+		
+	}
+	
+	@Test
+	public void Test2()
+	{
+		TypeOfTriangle triangle = new TypeOfTriangle(3, 4, 5);
+		
+		assertTrue( triangle.IsRight() );
+		
+		assertEquals( TriangleTypes.SCALENE, triangle.GetTypeOfTriangle() );
+		
+	}
+	
+	@Test
+	public void Test3()
+	{
+		TypeOfTriangle triangle = new TypeOfTriangle(5, 6, A);
+		//Code Needed
+	}
+	
+	@Test
+	public void Test4()
+	{
+		TypeOfTriangle triangle = new TypeOfTriangle(1, 2, 3);
+		
+		assertEquals( TriangleTypes.NONE, triangle.GetTypeOfTriangle());
+	}
+	
+	//Team 5 Tests
+	
+	@Test
+	public void TestHES1()
+	{
+		TypeOfTriangle triangle = new TypeOfTriangle(0, 1, 1);
+		
+		assertEquals( TriangleTypes.NONE, triangle.GetTypeOfTriangle());
+	}
+	
+	@Test
+	public void TestHES2() //Same as Group2
+	{
+		TypeOfTriangle triangle = new TypeOfTriangle(3, 4, 5);
+		
+		assertTrue( triangle.IsRight() );
+		
+		assertEquals( TriangleTypes.SCALENE, triangle.GetTypeOfTriangle() );
+	}
+	
+	@Test
+	public void TestHES3()
+	{
+		TypeOfTriangle triangle = new TypeOfTriangle(a, 4, $);
+		
+		//Code Needed
 	}
 }
