35763ecdf1f3168b8f9b5b704fab339da86bc5f1
Salvatore Amato <enptfb55@gmail.com>
2015-02-09 15:04:11 -0500

    fixed invalid input issue

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,29 +1,27 @@
 package main;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import triangle.TypeOfTriangle;
 
 
 
 public class Main {
+	
+	final static Scanner m_scanner = new Scanner(System.in);
+	final static Pattern m_pattern = Pattern.compile("^[0-9.]+$");
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner scan = new Scanner(System.in);
 		
-		System.out.println ( "Enter first side of your triangle:" );
+		String sSideA = GetPrompt( "Enter first side of your triangle:" );
 		
-		String sSideA = scan.next();
+		String sSideB = GetPrompt( "Enter second side of your triangle:" );
 		
-		System.out.println ( "Enter second side of your triangle:" );
-		
-		String sSideB = scan.next();
-		
-		System.out.println ( "Enter third side of your triangle:" );
-		
-		String sSideC = scan.next();
+		String sSideC = GetPrompt( "Enter third side of your triangle:" );
 		
 		double dSideA = Double.parseDouble( sSideA );
 		double dSideB = Double.parseDouble( sSideB );
@@ -39,4 +37,27 @@ public class Main {
 		
 	}
 
+	
+	private static String GetPrompt( String prompt )
+	{
+		String response = "";
+		
+		while( true )
+		{
+			System.out.println ( prompt );
+			
+			response = m_scanner.next();
+			
+			Matcher match = m_pattern.matcher( response );
+			
+			if ( match.find() )
+			{
+				break;
+			}
+			
+			System.out.println( response + " is not a valid response. Please try again.");
+		}
+		
+		return response;
+	}
 }
